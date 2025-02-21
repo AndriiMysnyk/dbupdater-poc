62eaf000d39bdb1d3c50d30a4520ceda71e6af62
Andrii Mysnyk <ondron@gmail.com>
2025-02-21 11:42:57 +0200

    Removing of SqlKata usages

--- DBUpdater.Console/Program.cs
+++ DBUpdater.Console/Program.cs
@@ -1,13 +1,8 @@
 ﻿using FluentMigrator.Runner;
 using DBUpdater.Common.SchemaLibrary;
 using DBUpdater.Migrations;
-using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
-using SqlKata;
-using SqlKata.Compilers;
-using SqlKata.Execution;
-
 using DBUpdater.Console;
 
 class Program
@@ -16,6 +11,16 @@ class Program
     private readonly static string schemaLibraryPath = @"..\Input\lib.json";
     private readonly static string migrationDescription = "First try to create tables";
     private readonly static long migrationVersion = 1;
+
+    static void Main(string[] args)
+    {
+        using ServiceProvider serviceProvider = CreateServices();
+        using IServiceScope scope = serviceProvider.CreateScope();
+
+        IMigrationRunner runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+
+        runner.MigrateUp();
+    }
 
     private static MigrationConfig CreateConfig()
     {
@@ -29,24 +34,6 @@ class Program
             data!.Tables);
 
         return config;
-    }
-
-    static void Main(string[] args)
-    {
-        using (var serviceProvider = CreateServices())
-        using (var scope = serviceProvider.CreateScope())
-        {
-            UpdateDatabase(scope.ServiceProvider);
-        }
-
-        ReadTables();
-    }
-
-    private static void UpdateDatabase(IServiceProvider serviceProvider)
-    {
-        var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
-
-        runner.MigrateUp();
     }
 
     private static ServiceProvider CreateServices()
@@ -63,24 +50,5 @@ class Program
                 .ScanIn(typeof(DynamicMigration).Assembly).For.Migrations())
             .AddLogging(lb => lb.AddFluentMigratorConsole())
             .BuildServiceProvider(false);
-    }
-
-    private static void ReadTables()
-    {
-        SqliteConnection connection = new(connectionString);
-
-        QueryFactory factory = new QueryFactory(connection, new SqliteCompiler());
-
-        Query query = factory
-            .Query()
-            .From("sqlite_master")
-            .Where("type", "table");
-
-        IEnumerable<dynamic> results = query.Get();
-
-        foreach (dynamic result in results)
-        {
-            Console.WriteLine(result);
-        }
     }
 }
